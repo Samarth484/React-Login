1747aa1f5620f4852dac391c90a63f796ad51f9c
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-06-25 15:13:04 +0530

    Refractored using Ternary Ops

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Login from "./Login";
-var isLoggedIn=false;
+var isLoggedIn = false;
 
-function renderPage(){
-  if(isLoggedIn === true)
-  return <h1 className="container">Hello User!</h1>
-  else return (  <div className="container">
-  <h1>Hello</h1>
- <Login></Login>
-</div>
-)
+function renderPage() {
+  return (isLoggedIn === true ? <h1 className="container">Hello User!</h1> : <div className="container">
+    <h1>Hello</h1>
+    <Login></Login>
+  </div>)
+
+
 }
 
 

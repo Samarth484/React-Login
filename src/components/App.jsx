f6e8bf3fe36efe6f702f0f778e33bfd132dc97d0
Samarth Srivastava <samarthsrivastava009@gmail.com>
2020-06-25 14:48:02 +0530

    First Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+var isLoggedIn=true;
+
+function renderPage(){
+  if(isLoggedIn === true)
+  return <h1 className="container">Hello User!</h1>
+  else return (  <div className="container">
+  <h1>Hello</h1>
+  <form className="form">
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <button type="submit">Login</button>
+  </form>
+</div>
+)
+}
+
+
+function App() {
+  return (
+    renderPage()
+  );
+}
+
+export default App;
